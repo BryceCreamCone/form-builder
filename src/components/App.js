ab00db5cb969b6b42c055643a5945f576a956406
Bryce Gust <bgust9@gmail.com>
2020-06-11 15:05:03 -0500

    Added styling and Link tags to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      Hiya!
+      <h1>Build-A-Form</h1>
+      <div className="home-btns">
+        <Link to="/forms/create" className="home-btn create-form-btn">+ Create A New Form</Link>
+        <br />
+        <Link to="/forms/:id" className="home-btn view-forms-btn">View Saved Forms</Link>
+      </div>
     </div>
   );
 }
